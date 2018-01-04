ad35ddd6da05819aa543d4172a3f12f577a4022a
Kate Schlunz <schlunzk@gmail.com>
2018-01-04 20:51:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,8 @@
 var playlist = new Object ({Bolton: "I can go the distance"});
+
+
+function updatePlaylist(playlist, artistName, songTitle){
+
+  return Object.assign({}, playlist, { [artistName]: songTitle })
+
+}
